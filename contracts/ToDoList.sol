1cbc1dcee25af2cadaf169030b0b4896b31a24a0
roshnins <roshnins@hotmail.com>
2022-09-14 16:52:25 +0800

    Listing Tasks in To Do List

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -6,28 +6,5 @@ contract ToDoList {
     // create a state variable that is written to the blockchain
     // tells the state of variable in blockchain
     // add 'public' to help create a function read this value from todolist
-    // state variable written to storage
     uint256 public taskCount = 0;
-
-    struct Task {
-        // unsigned integer
-        uint256 id;
-        string content;
-        bool completed;
-    }
-
-    // sort of like a database. int index returns task
-    mapping(uint256 => Task) public tasks;
-
-    constructor() public {
-        // add a default task to todo list
-        createTask("Smile a little today");
-    }
-
-    function createTask(string memory _content) public {
-        // add 1 to task count
-        taskCount++;
-        // put in the mapping
-        tasks[taskCount] = Task(taskCount, _content, false);
-    }
 }
